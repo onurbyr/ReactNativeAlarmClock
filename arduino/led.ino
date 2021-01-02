b65ea54c34014fae1d9dbdbcc16af49be57701f1
onurbyr <byronurr@gmail.com>
2021-01-02 04:05:07 +0300

    Led Off Timer 2

--- arduino/led.ino
+++ arduino/led.ino
@@ -1,15 +1,24 @@
+#include <virtuabotixRTC.h>
 #include <SoftwareSerial.h>
+
 SoftwareSerial mySerial(7,6); // RX,TX
 
-const int G = 9; 
-const int B = 10;     
-const int R = 11;
+const int G = 14; //AN0
+const int B = 15; //AN1    
+const int R = 16; //AN2
 
-String RGB = ""; 
+String Val = ""; 
 String RGB_Previous = "255,255,255"; 
 String ON = "ON"; 
 String OFF = "OFF"; 
-boolean RGB_Completed = false;
+boolean Val_Completed = false;
+//RTC
+virtuabotixRTC myRTC(8, 9, 10);
+
+
+int offSec=-1;
+int offMin=-1;
+int offHour=-1;
  
 void setup() {
   pinMode (R, OUTPUT);
@@ -20,7 +29,9 @@ void setup() {
   analogWrite(B, 255);
   Serial.begin(9600); 
   mySerial.begin(9600);
-  RGB.reserve(30);
+  Val.reserve(30);
+ // saniye, dakika, saat, haftanın kaçıncı günü olduğu, ayın kaçıncı günü olduğu, ay, yıl
+ //myRTC.setDS1302Time(0, 13, 2, 6, 2, 1, 2021); 
 }
  
 void loop() {
@@ -28,36 +39,148 @@ void loop() {
     char ReadChar = (char)mySerial.read();
  
     if(ReadChar == ')'){
-      RGB_Completed = true;
+      Val_Completed = true;
     }else{
-       RGB += ReadChar;
+       Val += ReadChar;
     }
   }
   
-  if(RGB_Completed){
-      if(RGB==ON){
-          RGB = RGB_Previous; 
+  if(Val_Completed){
+    //Light on
+      if(Val==ON){
+        
+          Val = RGB_Previous; 
           lightRGB();
-      }else if(RGB==OFF){
-          RGB = "0,0,0"; 
-          lightRGB();
-      }else{
-          lightRGB();   
-          RGB_Previous = RGB;     
       }
-      RGB = "";
-      RGB_Completed = false;      
-  } 
+      //light off
+      else if(Val==OFF){
+          Val = "0,0,0"; 
+          lightRGB();
+      }
+      //set timer
+      else if (Val.charAt(0)=='t')
+      {
+        ledTimer();
+        
+      }
+      //check timer
+      else if (Val.charAt(0)=='x')
+      {
+        if ((offHour==-1)||(offMin==-1)||(offSec==-1) )
+        {
+         
+        }
+        else 
+        {
+          int remainMin=-1;
+          int remainSec=-1;
+          if (offHour==myRTC.hours)
+          {
+            remainMin=offMin-myRTC.minutes;      
+          }
+          else
+          {
+            remainMin=offMin+(60-myRTC.minutes);   
+          }
+
+
+          if (offSec >=  myRTC.seconds)
+          {
+
+            remainSec=offSec-myRTC.seconds;
+            remainMin=remainMin+1;
+
+          }
+          else
+          {
+            remainSec=offSec+(60-myRTC.seconds);  
+          }
+
+   
+          mySerial.print(remainMin-1);
+          mySerial.print(",");
+          mySerial.print(remainSec);
+
+        }
+      }
+      //cancel timer
+       else if (Val.charAt(0)=='c')
+      {
+         offMin=-1;
+      }
+      else{
+          lightRGB();   
+          RGB_Previous = Val;     
+      }
+
+      
+      Val = "";
+      Val_Completed = false;      
+  }
+
+
+  doAlways();
+  
+
+
 } 
+
+
+void ledTimer(){
+  Val.remove(0,1);
+  int offGetMin=Val.toInt();
+  myRTC.updateTime();
+
+
+  offHour=myRTC.hours;
+  offMin=myRTC.minutes+offGetMin;
+  offSec=myRTC.seconds;
+  
+  if (offMin>=60)
+  {
+    offHour=offHour+1;
+    if (offHour>=24)
+    {
+      offHour=offHour-24;
+    }
+    offMin=offMin-60;
+  }
+  
+
+
+
+  }
+
+
+  void doAlways(){
+      myRTC.updateTime();
+
+
+    if ((offHour==myRTC.hours)&&(offMin==myRTC.minutes)&&(offSec==myRTC.seconds))
+    {
+      Val = "0,0,0";
+      RGB_Previous = "255,255,255"; 
+      lightRGB();
+      Val = "";
+      offHour=-1;
+      offMin=-1;
+      offSec=-1;
+    }
+    
+    
+    
+    }
+
+
  
 void lightRGB(){ 
-    int commaIndex = RGB.indexOf(',');
-    int secondCommaIndex = RGB.indexOf(',', commaIndex + 1);
-    int thirdCommaIndex = RGB.indexOf(',', secondCommaIndex + 1);
+    int commaIndex = Val.indexOf(',');
+    int secondCommaIndex = Val.indexOf(',', commaIndex + 1);
+    int thirdCommaIndex = Val.indexOf(',', secondCommaIndex + 1);
 
-    String firstValue = RGB.substring(0, commaIndex);
-    String secondValue = RGB.substring(commaIndex + 1, secondCommaIndex);
-    String thirdValue = RGB.substring(secondCommaIndex+1,thirdCommaIndex);
+    String firstValue = Val.substring(0, commaIndex);
+    String secondValue = Val.substring(commaIndex + 1, secondCommaIndex);
+    String thirdValue = Val.substring(secondCommaIndex+1,thirdCommaIndex);
 
     int rvalue = firstValue.toInt();
     int gvalue = secondValue.toInt();
--- src/pages/LedTimer.js
+++ src/pages/LedTimer.js
@@ -1,13 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { StyleSheet, Text, View,TouchableOpacity} from 'react-native'
 import InputSpinner from "react-native-input-spinner";
 import { color } from 'react-native-reanimated';
+import BluetoothSerial from 'react-native-bluetooth-serial'
 import IconMaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+
+
+function sendData(value){
+    BluetoothSerial.write(value)
+}
+
 
 
 
 function LedTimer() {
     const [number,setNumber] = useState(1);
+    const [minute,setMinute] = useState(-1);
+    const [second,setSecond] = useState(-1);
+    const [display,setDisplay] = useState("none");
+
+
+    useEffect(() => {
+        const toggle = setInterval(() => {
+            sendData("x)")
+            BluetoothSerial.readFromDevice().then((data) => {
+
+                var dataArr = data.split(',');
+
+                var remMin=dataArr[0];
+                var remSec=dataArr[1];
+
+                setMinute(remMin);
+                setSecond(remSec);
+
+            });
+
+            if (minute>=0 && second>=0 && second!=undefined)
+            {
+                setDisplay("flex")
+            }
+            else {
+                setDisplay("none")
+            }
+
+        }, 1000);
+   
+        return () => clearInterval(toggle);
+     })
+
+
+
     return (
         <View style={styles.container}>
         <Text style={styles.closeText}> Kapanma Süresini Belirle </Text>
@@ -23,11 +65,24 @@ function LedTimer() {
         style={styles.spinner}
         rounded={false}
         showBorder/>
-        <TouchableOpacity style={styles.offButton}>
+        <TouchableOpacity style={styles.offButton}
+        onPress={() => sendData("t"+number+")")}
+        >
             <IconMaterialCommunityIcons name="lightbulb-off"  size={50} color="#FF6A4F" />
             <Text style={styles.offButtonText} >{number} Dakika Sonra Işığı Kapat</Text>
         </TouchableOpacity>
+        <TouchableOpacity style={[styles.cancelButton,{display}]}
+        onPress={() => sendData("c)")}
+        >
+            <IconMaterialCommunityIcons name="cancel"  size={50} color="#E6707B" />
+            <Text style={styles.cancelButtonText} >Kapatma İşlemini İptal Et</Text>
+        </TouchableOpacity>
 
+        <Text style={[styles.textWillOff,{display}]}> 
+        {minute} Dakika {second} Saniye Sonra Işık Kapanacak
+        </Text>
+
+    
         </View>
     )
 }
@@ -44,14 +99,28 @@ const styles = StyleSheet.create({
         color:'#3D525F'
     },
     spinner: {
-        minWidth: 300,
-        marginBottom:8
+        minWidth: 300
     },
     offButton:{
+        marginTop:10,
         flexDirection:'row',
         alignItems:'center'
     },
     offButtonText:{
+        fontSize:16,
+        color:'#FF6A4F'
+    },
+    cancelButton:{
+        marginTop:20,
+        flexDirection:'row',
+        alignItems:'center'
+    },
+    cancelButtonText:{
+        fontSize:16,
+        color:'#E6707B'
+    },
+    textWillOff:{
+        marginTop:20,
         fontSize:16,
         color:'#FF6A4F'
     }
