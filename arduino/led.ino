439821f1194e6b476635e3b482146cd9258b805f
onurbyr <byronurr@gmail.com>
2021-01-03 02:05:09 +0300

    Led Br 2

--- arduino/led.ino
+++ arduino/led.ino
@@ -3,9 +3,9 @@
 
 SoftwareSerial mySerial(7,6); // RX,TX
 
-const int G = 14; //AN0
-const int B = 15; //AN1    
-const int R = 16; //AN2
+const int G = 3;
+const int B = 4;  
+const int R = 5;
 
 String Val = ""; 
 String RGB_Previous = "255,255,255"; 
@@ -19,6 +19,15 @@ virtuabotixRTC myRTC(8, 9, 10);
 int offSec=-1;
 int offMin=-1;
 int offHour=-1;
+
+float brightness;
+
+int rvalue=0;
+int gvalue=0;
+int bvalue=0;
+
+
+String B2 = "B2"; 
  
 void setup() {
   pinMode (R, OUTPUT);
@@ -50,11 +59,13 @@ void loop() {
       if(Val==ON){
         
           Val = RGB_Previous; 
+          commaIndex();
           lightRGB();
       }
       //light off
       else if(Val==OFF){
           Val = "0,0,0"; 
+          commaIndex();
           lightRGB();
       }
       //set timer
@@ -66,50 +77,25 @@ void loop() {
       //check timer
       else if (Val.charAt(0)=='x')
       {
-        if ((offHour==-1)||(offMin==-1)||(offSec==-1) )
-        {
-         
-        }
-        else 
-        {
-          int remainMin=-1;
-          int remainSec=-1;
-          if (offHour==myRTC.hours)
-          {
-            remainMin=offMin-myRTC.minutes;      
-          }
-          else
-          {
-            remainMin=offMin+(60-myRTC.minutes);   
-          }
-
-
-          if (offSec >=  myRTC.seconds)
-          {
-
-            remainSec=offSec-myRTC.seconds;
-            remainMin=remainMin+1;
-
-          }
-          else
-          {
-            remainSec=offSec+(60-myRTC.seconds);  
-          }
-
-   
-          mySerial.print(remainMin-1);
-          mySerial.print(",");
-          mySerial.print(remainSec);
-
-        }
+        checkTimer();
       }
       //cancel timer
        else if (Val.charAt(0)=='c')
       {
          offMin=-1;
       }
+      //brightness
+       else if (Val.charAt(0)=='b')
+      {
+         ledBrightness();
+      }
+      else if (Val==B2)
+      {
+         mySerial.print(brightness);
+      }
       else{
-          lightRGB();   
+          commaIndex();
+          lightRGB();  
           RGB_Previous = Val;     
       }
 
@@ -146,10 +132,70 @@ void ledTimer(){
     offMin=offMin-60;
   }
   
+  }
+
+
+  void checkTimer()
+  {
+    if ((offHour==-1)||(offMin==-1)||(offSec==-1) )
+    {
+         
+    }
+    else 
+    {
+      int remainMin=-1;
+      int remainSec=-1;
+      if (offHour==myRTC.hours)
+    {
+      remainMin=offMin-myRTC.minutes;      
+    }
+    else
+    {
+    remainMin=offMin+(60-myRTC.minutes);   
+    }
+    if (offSec >=  myRTC.seconds)
+    {
+      remainSec=offSec-myRTC.seconds;
+      remainMin=remainMin+1;
+    }
+    else
+    {
+      remainSec=offSec+(60-myRTC.seconds);
+    }
+    mySerial.print(remainMin-1);
+    mySerial.print(",");
+    mySerial.print(remainSec);
+    }    
+    
+
+  }
+
+  
+
+  void ledBrightness()
+  {
+    Val.remove(0,1);
+    brightness=float(Val.toInt());
+    
+    Val = RGB_Previous; 
+
+
+    commaIndex();
+    
+    rvalue=(float)(brightness/100)*rvalue;
+    gvalue=(float)(brightness/100)*gvalue;
+    bvalue=(float)(brightness/100)*bvalue;
+
+
+    lightRGB();
 
 
 
   }
+
+
+
+
 
 
   void doAlways(){
@@ -160,6 +206,7 @@ void ledTimer(){
     {
       Val = "0,0,0";
       RGB_Previous = "255,255,255"; 
+      commaIndex();
       lightRGB();
       Val = "";
       offHour=-1;
@@ -172,8 +219,10 @@ void ledTimer(){
     }
 
 
- 
-void lightRGB(){ 
+
+  
+  void commaIndex()
+  {
     int commaIndex = Val.indexOf(',');
     int secondCommaIndex = Val.indexOf(',', commaIndex + 1);
     int thirdCommaIndex = Val.indexOf(',', secondCommaIndex + 1);
@@ -182,10 +231,16 @@ void lightRGB(){
     String secondValue = Val.substring(commaIndex + 1, secondCommaIndex);
     String thirdValue = Val.substring(secondCommaIndex+1,thirdCommaIndex);
 
-    int rvalue = firstValue.toInt();
-    int gvalue = secondValue.toInt();
-    int bvalue = thirdValue.toInt();
+    rvalue = firstValue.toInt();
+    gvalue = secondValue.toInt();
+    bvalue = thirdValue.toInt();
+    
+  }
 
+
+
+ 
+  void lightRGB(){ 
     analogWrite(R, 255-rvalue);
     analogWrite(G, 255-gvalue);
     analogWrite(B, 255-bvalue);
--- src/pages/LedBrightness.js
+++ src/pages/LedBrightness.js
@@ -1,33 +1,63 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React, { useState,useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity,ToastAndroid } from 'react-native'
 import Slider from '@react-native-community/slider';
 import IconMaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import BluetoothSerial from 'react-native-bluetooth-serial'
+
+function sendData(value){
+    BluetoothSerial.write(value)
+}
 
 
 
 function LedBrightness() {
-    const [brightness,setBrightness] = useState(1);
+    const [brightness,setBrightness] = useState(100);
+
+    useEffect(() => {
+        sendData("B2)")
+        setTimeout(function()
+        {
+            BluetoothSerial.readFromDevice().then((data) => {
+
+                var br = ~~data;
+                setBrightness(br)
+
+            });
+        }, 300);
+ 
+    }, []);
+
+
     return (
         <View style={styles.container}>
             <Text style={styles.brText}>Led Parlaklığını Ayarla</Text>
             <Slider
-            style={{width: 250, height: 75 }}
-            minimumValue={0}
-            maximumValue={250}
+            style={{width: 300, height: 75 }}
+            minimumValue={1}
+            maximumValue={100}
             step={1}
-            minimumTrackTintColor="#2D6C9C"
+            minimumTrackTintColor="#FED842"
             maximumTrackTintColor="red"
-            thumbTintColor={'#2D6C9C'}
-            //thumbImage={require('../images/slider.png')}
-            onSlidingComplete={(value) =>setBrightness(value)}
-            />
-            <TouchableOpacity onPress={() => alert(brightness)}><Text>dddd</Text></TouchableOpacity>
-       
-        
+            // thumbTintColor={'#2D6C9C'}
+            thumbImage={require('../images/brightness.png')}
+            value={brightness}
+            onSlidingComplete={value =>setBrightness(value)}
+            />  
+            <TouchableOpacity style={styles.brButton}
+            onPress={() => {
+             sendData("b"+brightness+")")
+             ToastAndroid.show('Parlaklık Ayarlandı', ToastAndroid.SHORT);
+             }}>
+            <IconMaterialCommunityIcons name="brightness-6"  size={40} color="#FF9A8A" />
+            <Text style={styles.brButtonText} >Parlaklık Ayarla</Text>
+            </TouchableOpacity>
+
             
         </View>
     )
 }
+
+
 
 const styles = StyleSheet.create({
     container: {
@@ -36,11 +66,17 @@ const styles = StyleSheet.create({
         justifyContent:'center',
       },
       brText:{
-          color:'#2E6D9C',
+          color:'#8D8B8B',
           fontSize:20
+      },
+      brButton:{
+        flexDirection:'row',
+        alignItems:'center'
+      },
+      brButtonText:{
+        fontSize:16,
+        color:'#8D8B8B'
       }
-
-
 
 })
 
